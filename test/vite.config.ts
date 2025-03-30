81f1a9f4567791c9f6b6aa05f959d369a78644bf
Eduardo Magaldi <magaldi1989@gmail.com>
2025-03-30 19:33:52 -0300

    getting a new vite working into ghpages

--- test/vite.config.ts
+++ test/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'resume',
+  },
+  base: '/resume/',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
   },
 })
